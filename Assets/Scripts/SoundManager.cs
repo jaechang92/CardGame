89c4d7acfcd33dd89bf822e37b10d023dd3ba6a9
jaechang <jaechang0721k@gmail.com>
2019-10-27 15:45:13 +0900

    Read me

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public List<AudioClip> BackGroundSound;
+    public List<AudioClip> EffectSound;
+
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public List<GameObject> UI;
+
+	// Use this for initialization
+	void Start () {
+        UI[1].SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void SelectBtn()
+    {
+        UI[0].SetActive(false);
+        UI[1].SetActive(true);
+    }
+
+}
