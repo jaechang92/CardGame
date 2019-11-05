5f592b130f98f9005ddc9ca19d396e2187bccb88
jaechang <jaechang0721k@gmail.com>
2019-11-05 22:06:16 +0900

    Card

---
+++ Assets/Scripts/CardNameAndOption.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardNameAndOption : MonoBehaviour {
+
+    public RectTransform tr;
+
+
+    public Vector2 sizeVertical;
+    public Vector2 sizeHorizontal;
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<RectTransform>();
+        sizeVertical = new Vector2(134, 200);
+        sizeHorizontal = new Vector2(150, 20);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (tr.anchoredPosition.x > 255)
+        {
+            tr.sizeDelta = sizeHorizontal;
+        }
+        else
+        {
+            tr.sizeDelta = sizeVertical;
+        }
+	}
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,15 +7,36 @@ using UnityEngine.EventSystems;
 public class UIManager : MonoBehaviour {
 
     public List<GameObject> UI;
+    public List<GameObject> Page;
 
-	// Use this for initialization
-	void Start () {
+
+    public GameObject cardDrag;
+    public bool dragging = false;
+    private int temp = 0;
+
+    void Init()
+    {
         UI[1].SetActive(false);
+        cardDrag.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Init();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (dragging)
+        {
+            cardDrag.SetActive(true);
+            cardDrag.transform.position = Input.mousePosition;
+        }
+        else
+        {
+            cardDrag.SetActive(false);
+        }
 	}
 
 
@@ -33,6 +54,7 @@ public class UIManager : MonoBehaviour {
 
     public void CardSelectArrowButton(string A)
     {
+        
         switch (A)
         {
             case "Right":
@@ -41,7 +63,10 @@ public class UIManager : MonoBehaviour {
                 break;
 
             case "Left":
-                EventSystem.current.currentSelectedGameObject.transform.parent.GetComponent<Animator>().SetTrigger("Open");
+                
+                temp = int.Parse(EventSystem.current.currentSelectedGameObject.transform.parent.name);
+                Page[temp-1].GetComponent<Animator>().SetTrigger("Open");
+                
                 break;
 
             default:
@@ -49,5 +74,23 @@ public class UIManager : MonoBehaviour {
         }
     }
     
+    public void CardDarg(int i)
+    {
+        Debug.Log("카드"+i);
+    }
+
+
+
+
+    public void StartDrag()
+    {
+        dragging = true;
+    }
+
+    public void EndDrag()
+    {
+        dragging = false;
+    }
+
 
 }
